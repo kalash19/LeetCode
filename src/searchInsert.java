bd6576ebdebf0b2fbac4348d3da2465ea59e559c
Dell <kalash.khatiyan_cs19@gla.ac.in>
2022-02-26 23:20:57 +0530

    Initial Commit

---
+++ src/searchInsert.java
@@ -0,0 +1,18 @@
+public class searchInsert {
+    class Solution {
+        public int searchInsert(int[] nums, int target) {
+            int n=nums.length;
+            for(int i = 0; i < nums.length; i++)
+
+
+                if (nums[i] == target)
+                    return i;
+
+
+                else if (nums[i] > target)
+                    return i;
+
+            return n;
+        }
+    }
+}
