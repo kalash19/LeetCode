6cfc48108a525b721a7efed4f558935bb29b9955
Dell <kalash.khatiyan_cs19@gla.ac.in>
2022-03-11 23:12:48 +0530

    Initial Commit

---
+++ src/xoroperation.java
@@ -0,0 +1,17 @@
+public class xoroperation {
+    class Solution {
+        public int xorOperation(int n, int start) {
+            int[] ar = new int[n];
+            for(int i = 0; i < n; i++) {
+                ar[i] = start + 2*i;
+            }
+
+            int result = start;
+            for(int i = 0; i < n-1; i++) {
+                result = result ^ ar[i+1];
+            }
+            return result;
+
+        }
+    }
+}
